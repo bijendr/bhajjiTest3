27570ecd1d3449adfd6153f3580c05c625888329
cyberlinksIOS <bij>
2015-08-27 15:35:13 +0530

    first commit 3

---
+++ bijendraTest/bijendraTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bijendraTest
+//
+//  Created by Bijendra Singh Bhatia on 27/08/15.
+//  Copyright (c) 2015 Blissbooker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+// bijendra
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
